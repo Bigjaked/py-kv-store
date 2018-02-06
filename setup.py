78f03afb9482294ad8c0a2bb8df25cf84542b6e0
Jacob Duncan <duncan.jacob.mk@gmail.com>
2018-02-06 16:56:51 -0700

    update binary

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 ext_modules = [
-    Extension("persist_kv_store.stores.cache.cache",
+    Extension("Persist-KV.persist_kv_store.stores.cache.cache",
               ["persist_kv_store\\stores\\cache\\cache.pyx"]),
-    Extension("persist_kv_store.stores.basestore",
+    Extension("Persist-KV.persist_kv_store.stores.basestore",
               ["persist_kv_store\\stores\\basestore.py"]),
-    Extension("persist_kv_store.stores.memstore",
+    Extension("Persist-KV.persist_kv_store.stores.memstore",
               ["persist_kv_store\\stores\\memstore.py"]),
-    Extension("persist_kv_store.stores.redisstore",
+    Extension("Persist-KV.persist_kv_store.stores.redisstore",
               ["persist_kv_store\\stores\\redisstore.py"]),
-    Extension("persist_kv_store.stores.persiststore",
+    Extension("Persist-KV.persist_kv_store.stores.persiststore",
               ["persist_kv_store\\stores\\persiststore.py"]),
-    Extension("persist_kv_store.stores.serializer",
+    Extension("Persist-KV.persist_kv_store.stores.serializer",
               ["persist_kv_store\\stores\\serializer.py"]),
 ]
 
