d9ba2208bfa3f5edc90a328cb65f0f5f0ac90bd7
Jacob Duncan <duncan.jacob.mk@gmail.com>
2018-02-06 16:53:15 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 ext_modules = [
-    Extension(
-        "persist_kv_store.stores.cache.cache", ["persist_kv_store\stores\cache\cache.pyx"]),
-    Extension(
-        "persist_kv_store.stores.basestore", ["persist_kv_store\stores\\basestore.py"]),
-    Extension(
-        "persist_kv_store.stores.memstore", ["persist_kv_store\stores\memstore.py"]),
-    Extension(
-        "persist_kv_store.stores.redisstore", ["persist_kv_store\stores\\redisstore.py"]),
-    Extension(
-        "persist_kv_store.stores.persiststore", ["persist_kv_store\stores\persiststore.py"]),
-    Extension(
-        "persist_kv_store.stores.serializer", ["persist_kv_store\stores\serializer.py"]),
+    Extension("persist_kv_store.stores.cache.cache",
+              ["persist_kv_store\\stores\\cache\\cache.pyx"]),
+    Extension("persist_kv_store.stores.basestore",
+              ["persist_kv_store\\stores\\basestore.py"]),
+    Extension("persist_kv_store.stores.memstore",
+              ["persist_kv_store\\stores\\memstore.py"]),
+    Extension("persist_kv_store.stores.redisstore",
+              ["persist_kv_store\\stores\\redisstore.py"]),
+    Extension("persist_kv_store.stores.persiststore",
+              ["persist_kv_store\\stores\\persiststore.py"]),
+    Extension("persist_kv_store.stores.serializer",
+              ["persist_kv_store\\stores\\serializer.py"]),
 ]
 
 setup(
