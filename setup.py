37f8edb68637b05e26c3e8302d8ef541859cfa1f
Jacob Duncan <duncan.jacob.mk@gmail.com>
2018-02-06 17:00:51 -0700

    update binary

--- setup.py
+++ setup.py
@@ -2,24 +2,25 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+VERSION = '0.9'
 ext_modules = [
-    Extension("Persist-KV.persist_kv_store.stores.cache.cache",
-              ["persist_kv_store\\stores\\cache\\cache.pyx"]),
-    Extension("Persist-KV.persist_kv_store.stores.basestore",
-              ["persist_kv_store\\stores\\basestore.py"]),
-    Extension("Persist-KV.persist_kv_store.stores.memstore",
-              ["persist_kv_store\\stores\\memstore.py"]),
-    Extension("Persist-KV.persist_kv_store.stores.redisstore",
-              ["persist_kv_store\\stores\\redisstore.py"]),
-    Extension("Persist-KV.persist_kv_store.stores.persiststore",
-              ["persist_kv_store\\stores\\persiststore.py"]),
-    Extension("Persist-KV.persist_kv_store.stores.serializer",
-              ["persist_kv_store\\stores\\serializer.py"]),
+    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.cache.cache",
+              [f"persist_kv_store\\stores\\cache\\cache.pyx"]),
+    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.basestore",
+              [f"persist_kv_store\\stores\\basestore.py"]),
+    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.memstore",
+              [f"persist_kv_store\\stores\\memstore.py"]),
+    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.redisstore",
+              [f"persist_kv_store\\stores\\redisstore.py"]),
+    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.persiststore",
+              [f"persist_kv_store\\stores\\persiststore.py"]),
+    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.serializer",
+              [f"persist_kv_store\\stores\\serializer.py"]),
 ]
 
 setup(
     name="Persist-KV",
-    version='0.9',
+    version=VERSION,
     description="Python key value persistance mechanism",
     author='Jacob Duncan',
     author_email='jacob.duncan@truepowere.com',
