8a2c285b684ee6221630af4791877c3988c5aa6a
Jacob Duncan <duncan.jacob.mk@gmail.com>
2018-02-06 17:03:10 -0700

    update binary

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@ from Cython.Build import cythonize
 
 VERSION = '0.9'
 ext_modules = [
-    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.cache.cache",
-              [f"persist_kv_store\\stores\\cache\\cache.pyx"]),
-    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.basestore",
-              [f"persist_kv_store\\stores\\basestore.py"]),
-    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.memstore",
-              [f"persist_kv_store\\stores\\memstore.py"]),
-    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.redisstore",
-              [f"persist_kv_store\\stores\\redisstore.py"]),
-    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.persiststore",
-              [f"persist_kv_store\\stores\\persiststore.py"]),
-    Extension(f"Persist-KV-{VERSION}.persist_kv_store.stores.serializer",
-              [f"persist_kv_store\\stores\\serializer.py"]),
+    Extension("persist_kv_store.stores.cache.cache",
+              ["persist_kv_store\\stores\\cache\\cache.pyx"]),
+    Extension("persist_kv_store.stores.basestore",
+              ["persist_kv_store\\stores\\basestore.py"]),
+    Extension("persist_kv_store.stores.memstore",
+              ["persist_kv_store\\stores\\memstore.py"]),
+    Extension("persist_kv_store.stores.redisstore",
+              ["persist_kv_store\\stores\\redisstore.py"]),
+    Extension("persist_kv_store.stores.persiststore",
+              ["persist_kv_store\\stores\\persiststore.py"]),
+    Extension("persist_kv_store.stores.serializer",
+              ["persist_kv_store\\stores\\serializer.py"]),
 ]
 
 setup(
-    name="Persist-KV",
+    name="persist_kv_store",
     version=VERSION,
     description="Python key value persistance mechanism",
     author='Jacob Duncan',
