0a16d7e6b61bf994a797b104647b52ef91feaad5
Jacob Duncan <duncan.jacob.mk@gmail.com>
2018-03-28 19:50:12 -0600

    skip serializer for NONE

--- persist_kv_store/stores/cached_vedis.pyx
+++ persist_kv_store/stores/cached_vedis.pyx
@@ -2,35 +2,44 @@
 
 import vedis
 from . import DEFAULT_CACHE_LIMIT
+from .serializer import KeyValueSerializer
 cimport cache
 
 
 cdef class CachedVedis(cache.CacheMixin):
+
     cdef str file_location
     cdef object _vedis_db
     def __init__(self, file_=':mem:', limit_=DEFAULT_CACHE_LIMIT):
         cache.CacheMixin.__init__(self, limit=limit_)
+        self._serializer = KeyValueSerializer()
         self.file_location = file_
         self._vedis_db = vedis.Vedis(self.file_location)
 
     cdef set_(self, str key, object value):
         self._set_cached(key, value)
-        self._vedis_db[key] = value
+        packed = self._serializer.serialize(value)
+        self._vedis_db[key] = packed
 
     cdef get_(self, str key):
         if key in self._cache:
             return self._get_cached(key)
         cdef object packed = self._vedis_db[key]
         if packed:
-            return packed
-        return 'None'
+            p = self._serializer.deserialize(self._vedis_db.get(key))
+            self._set_cached(key, p)
+            return p
+        return None
+
     def set(self, str key, object value):
         self.set_(key, value)
+
     def get(self, key):
         return self.get_(key)
 
     def __setitem__(self, str key, object value):
         self.set_(key, value)
+
     def __getitem__(self, str item):
         return self.get_(item)
 
